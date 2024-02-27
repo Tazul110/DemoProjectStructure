a17279bcc1b50c8663741f8c93995a7a3c09de8e
Tazul-islam <it49tazulislam@gmail.com>
2024-02-27 18:20:49 +0600

    first commit

--- Demo_Project/Demo_Project/obj/Debug/net8.0/Demo_Project.AssemblyInfo.cs
+++ Demo_Project/Demo_Project/obj/Debug/net8.0/Demo_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f3fe7f0ee75d819192a87f05bb505030ace724f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
